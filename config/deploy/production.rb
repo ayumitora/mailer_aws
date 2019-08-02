e4b7e35603afb8d0a99166ae9750235cbafff100
ayumi_arai <0araiayumi@gmail.com>
2019-08-02 13:51:02 +0900

    デプロイ前コミット

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,5 @@
 server '3.114.91.47', user: 'app', roles: %w{app db web}
-set :ssh_options, keys: '~/.ssh/id_rsa.pub'
+# set :ssh_options, keys: '~/.ssh/id_rsa.pub'
 
+set :ssh_options, {keys: %w(~/.ssh/id_rsa)}
 # set :ssh_options, {keys: [File.expand_path('~/.ssh/id_rsa.pub')]}
